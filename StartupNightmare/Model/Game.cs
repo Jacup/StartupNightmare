b1445d30eb97cf5bcf7777e023c13733efafd9de
Jakub Gramburg <jacubgr@gmail.com>
2022-09-17 21:51:29 +0200

    some base classes for future development

---
+++ StartupNightmare/Model/Game.cs
@@ -0,0 +1,33 @@
+﻿using StartupNightmare.Model.People;
+
+namespace StartupNightmare.Model
+{
+    public class Game
+    {        
+        public List<Player>? players;
+
+        private static Game? _instance;
+        private static readonly object _lock = new();
+
+        private Game() { }
+
+        public static Game GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    _instance ??= new Game();
+                }
+            }
+
+            return _instance;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("List of Players:");
+            Console.WriteLine(players);
+        }
+    }
+}
